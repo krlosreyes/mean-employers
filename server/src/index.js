b87af7d854432e4e4571d6c378a59708807380ac
Charlie <caxireit@gmail.com>
2021-02-05 20:04:45 -0500

    version01

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,6 @@
-console.log("Hola mensos");
+const express = require('express') //se requiere express
+
+const app = express()// se crea la constante app
+
+app.listen(3000);// Se le indica el puerto donde debe escuchar las peticiones
+console.log('server on port', 3000);
